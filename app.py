76ffa7afb7e1a14e8ced1ad8fa8e939001040964
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-04-19 07:00:54 +0000

    added /bookshelf route

--- app.py
+++ app.py
@@ -24,11 +24,22 @@ def genersPage():
             return result2
         else:
             return error(result2.json(),result2.status_code)
-    
 
-@app.route('/video/<id>')
-def videos(id):
-    return f'Stai guardando il video {id}'
+@app.route('/bookshelf')
+def bookshelfPage():
+    bookshelfId = request.args.get('bookshelfId') 
+    if not bookshelfId:
+        result = requests.get('https://librarymanagementpw.azurewebsites.net/api/BookShelf')
+        if (result.status_code == 200):
+            return render_template('bookshelf.html', json=result.json() , len=len(result.json()))
+        else:
+            return error(result.json(),result.status_code)
+    else:
+        result = requests.get(f'https://librarymanagementpw.azurewebsites.net/api/BookShelf/{bookshelfId}')
+        if (result.status_code == 200):
+            return render_template('bookshelfsng.html', json=result.json() , len=len(result.json()))
+        else:
+            return error(result.json(),result.status_code)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
