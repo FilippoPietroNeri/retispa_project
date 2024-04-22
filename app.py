2f8870335e73d4944e2ad440af669442d9d997b8
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-04-22 09:02:36 +0000

    Added Bootstrap and others

--- app.py
+++ app.py
@@ -11,19 +11,11 @@ def homepage():
 
 @app.route('/genres')
 def genersPage():
-    genreId = request.args.get('genreId') 
-    if not genreId:
-        result = requests.get('https://librarymanagementpw.azurewebsites.net/api/genre')
-        if (result.status_code == 200):
-            return render_template('genres.html', json=result.json() , len=len(result.json()))
-        else:
-            return error(result.json(),result.status_code)
+    result = requests.get('https://librarymanagementpw.azurewebsites.net/api/genre')
+    if (result.status_code == 200):
+        return render_template('genres.html', json=result.json() , len=len(result.json()))
     else:
-        result2 = requests.get(f'https://librarymanagementpw.azurewebsites.net/api/genre/{genreId}')
-        if (result2.status_code == 200):
-            return result2
-        else:
-            return error(result2.json(),result2.status_code)
+        return error(result.json(),result.status_code)
 
 @app.route('/bookshelf')
 def bookshelfPage():
@@ -40,6 +32,10 @@ def bookshelfPage():
             return render_template('bookshelfsng.html', json=result.json() , len=len(result.json()))
         else:
             return error(result.json(),result.status_code)
+
+@app.route('/search/book')
+def srcbook():
+    return render_template('searchbook.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
